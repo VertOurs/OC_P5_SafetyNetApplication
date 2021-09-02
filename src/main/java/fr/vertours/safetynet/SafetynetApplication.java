aa2a99edc9d44da3e88173e91ac45c143bb2e8c7
Ours <vertours.dev@gmail.com>
2021-09-02 15:35:47 +0200

    CODE -- getter et setter d' FireStation controller

--- src/main/java/fr/vertours/safetynet/SafetynetApplication.java
+++ src/main/java/fr/vertours/safetynet/SafetynetApplication.java
@@ -1,7 +1,17 @@
 package fr.vertours.safetynet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.vertours.safetynet.model.FireStation;
+import fr.vertours.safetynet.service.FireStationService;
+import org.objectweb.asm.TypeReference;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @SpringBootApplication
 public class SafetynetApplication {
@@ -9,5 +19,23 @@ public class SafetynetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SafetynetApplication.class, args);
 	}
+/*
+	@Bean
+	CommandLineRunner runner(FireStationService fireStationService){
+		return args -> {
+			ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<FireStation>> typeReference = new TypeReference<List<FireStation>>(){};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/data.json");
+			try {
+				List<FireStation> fireStations = mapper.readValue(inputStream, typeReference);
+				FireStationService.create(firestations);
+			} catch (IOException e){
+				System.out.println(e.getMessage());
+			}
+		};
+
+	}*/
+
+	//JsonIterator.deserialize("[1,2,3]", map.class);
 
 }
--- src/main/java/fr/vertours/safetynet/controller/FireStationController.java
+++ src/main/java/fr/vertours/safetynet/controller/FireStationController.java
@@ -2,6 +2,7 @@ package fr.vertours.safetynet.controller;
 
 import fr.vertours.safetynet.model.FireStation;
 import fr.vertours.safetynet.repository.FireStationRepository;
+import fr.vertours.safetynet.service.FireStationService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,4 +10,13 @@ import java.util.List;
 @RestController
 public class FireStationController {
 
+    private FireStationService fireStationService;
+
+    public FireStationService getFireStationService() {
+        return fireStationService;
+    }
+
+    public void setFireStationService(FireStationService fireStationService) {
+        this.fireStationService = fireStationService;
+    }
 }
